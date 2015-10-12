684a301a04561b4022e8a35d05788681cd8d3e1e
rhysd <lin90162@yahoo.co.jp>
2015-10-13 00:33:24 +0900

    support cookie

--- renderer/index.jsx
+++ renderer/index.jsx
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux'
 import tilectron from './reducers'
 import App from './components/app.jsx'
 
+require('electron-cookies');
+
 let store = createStore(tilectron);
 
 ReactDOM.render(
