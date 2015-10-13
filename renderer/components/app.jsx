045d6f191fa33e42a344e16e221e183f7e749ee1
rhysd <lin90162@yahoo.co.jp>
2015-10-13 15:33:59 +0900

    fix length of elements

--- renderer/components/app.jsx
+++ renderer/components/app.jsx
@@ -9,11 +9,17 @@ import DebugButtons from './debug-buttons.jsx'
 class App extends React.Component {
     renderTree() {
         const {dispatch, tree, current_id, views} = this.props;
+        const common_props = {
+            style: {flex: 'auto'},
+            current_id,
+            views,
+            dispatch
+        };
 
         if (tree instanceof TileLeaf) {
-            return <Tile leaf={tree} current_id={current_id} views={views} dispatch={dispatch}/>;
+            return <Tile leaf={tree} {...common_props}/>;
         } else {
-            return <Container knot={tree} current_id={current_id} views={views} dispatch={dispatch}/>;
+            return <Container knot={tree} {...common_props}/>;
         }
     }
 
