eb7bc0038cda295628894a7f152a69b590f14a79
rhysd <lin90162@yahoo.co.jp>
2015-10-17 02:22:33 +0900

    add missing web-page.jsx

---
+++ renderer/components/web-page.jsx
@@ -0,0 +1,31 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class WebPage extends Component {
+    componentDidMount() {
+        this.mountWebView();
+    }
+
+    componentDidUpdate() {
+        this.mountWebView();
+    }
+
+    mountWebView() {
+        const {focused, webview} = this.props;
+        this.refs.body.appendChild(webview);
+        if (focused) {
+            webview.focus();
+        }
+    }
+
+    render() {
+        return (
+            <div className="web-page" ref="body">
+            </div>
+        );
+    }
+}
+
+WebPage.propTypes = {
+    focused: PropTypes.bool,
+    webview: PropTypes.object
+};
