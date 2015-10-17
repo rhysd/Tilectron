acb8ca85e06b5c7b6d646de7372a668b8150ba5a
rhysd <lin90162@yahoo.co.jp>
2015-10-17 17:19:40 +0900

    make <Tile> component stateless

--- renderer/components/tile.jsx
+++ renderer/components/tile.jsx
@@ -1,60 +1,42 @@
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
 import AddressBar from './address-bar.jsx';
 import OmniInput from './omni-input.jsx';
 import WebPage from './web-page.jsx';
 import {changeFocus} from '../actions';
 
-export default class Tile extends Component {
-
-    isFocused() {
-        const {current_id, leaf} = this.props;
-        return current_id === leaf.id;
+function renderAddressBar(dispatch, leaf, focused) {
+    if (!focused) {
+        return undefined;
     }
 
-    getClass() {
-        return this.isFocused() ? 'tile focused' : 'tile';
-    }
+    return (
+        <div className="addr-bar-wrapper animated fadeInUp">
+            <AddressBar dispatch={dispatch} tileId={leaf.id}/>
+        </div>
+    );
+}
 
-    renderAddressBar(dispatch, leaf) {
-        if (!this.isFocused()) {
-            return undefined;
-        }
-
+function renderContent(page, dispatch, leaf, focused) {
+    if (page) {
+        return <WebPage webview={page.webview} focused={focused}/>;
+    } else {
         return (
-            <div className="addr-bar-wrapper animated fadeInUp">
-                <AddressBar dispatch={dispatch} tileId={leaf.id}/>
-            </div>
-        );
-    }
-
-    renderContent(page, dispatch, leaf) {
-        if (page) {
-            return <WebPage webview={page.webview} focused={this.isFocused()}/>;
-        } else {
-            return (
-                <div className="new-window">
-                    <OmniInput dispatch={dispatch} tileId={leaf.id} autoFocus/>
-                </div>
-            );
-        }
-    }
-
-    render() {
-        const {leaf, pages, dispatch, style} = this.props;
-        const page = pages[leaf.id];
-        return (
-            <div className={this.getClass()} style={style} onMouseOver={() => dispatch(changeFocus(leaf.id))}>
-                {this.renderContent(page, dispatch, leaf)}
-                {this.renderAddressBar(dispatch, leaf)}
+            <div className="new-window">
+                <OmniInput dispatch={dispatch} tileId={leaf.id} autoFocus/>
             </div>
         );
     }
 }
 
-Tile.propTypes = {
-    current_id: PropTypes.number,
-    dispatch: PropTypes.func,
-    leaf: PropTypes.object,
-    pages: PropTypes.object,
-    style: PropTypes.object
-};
+export default function Tile(props) {
+    const {current_id, leaf, pages, dispatch, style} = props;
+    const focused = current_id === leaf.id;
+    const name = focused ? 'tile focused' : 'tile';
+    const page = pages[leaf.id];
+    return (
+        <div className={name} style={style} onMouseOver={() => dispatch(changeFocus(leaf.id))}>
+            {renderContent(page, dispatch, leaf, focused)}
+            {renderAddressBar(dispatch, leaf, focused)}
+        </div>
+    );
+}
